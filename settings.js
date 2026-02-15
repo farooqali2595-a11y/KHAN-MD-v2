b7d10368358661b1b39d4813aee29ce59488b6e0
farooqali2595-a11y <farooqali2595@gmail.com>
2026-02-15 11:57:52 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  SESSION_ID: "IK~CIUSzKCK#XGwkF1HvRxz6arGQ7BNAd9Eo2EMRC1gox2NsoDz6_y0",  // add session Id here
+  
+  OWNER_NUMBER: "447596060651", // add owner number 
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Asia/Karachi" // put your country timeZone....leave blank if u don't know.
+};
